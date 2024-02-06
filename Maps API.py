fbaf2a2c946f1efb8542d80a0dd112516724eeb9
noskov.is0042 <ivann400@yandex.ru>
2024-02-06 16:14:21 +0300

    Большая задача по Maps API. Часть №2

--- Maps API.py
+++ Maps API.py
@@ -1,5 +1,6 @@
+import pygame
 from Samples.geocoder import get_coordinates, get_ll_span
-from Samples.mapapi_PG import show_map
+from Samples.mapapi_PG import return_map
 
 argv = ['Maps API.py', '37.617848, 55.755336']
 
@@ -7,7 +8,23 @@ toponym_to_find = " ".join(argv[1:])
 
 if toponym_to_find:
     ll, spn = get_ll_span(toponym_to_find)
-    ll_spn = f"ll={ll}&spn={spn}"
-    show_map(ll_spn, 'map')
+    yandex_map = return_map(f"ll={ll}&spn={spn}", 'map')
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+
+    screen.blit(yandex_map, (0, 0))
+    while pygame.event.wait().type != pygame.QUIT:
+        key = pygame.key.get_pressed()
+        if key[pygame.K_PAGEUP]:
+            spn = ','.join([str(float(i) * 1.5) for i in spn.split(',')])
+            yandex_map = return_map(f"ll={ll}&spn={spn}", 'map')
+        if key[pygame.K_PAGEDOWN]:
+            spn = ','.join([str(float(i) / 1.5) for i in spn.split(',')])
+            yandex_map = return_map(f"ll={ll}&spn={spn}", 'map')
+        screen.fill('White')
+        screen.blit(yandex_map, (0, 0))
+        pygame.display.flip()
+    pygame.quit()
 else:
     print('Данных нет')
--- Samples/mapapi_PG.py
+++ Samples/mapapi_PG.py
@@ -42,3 +42,32 @@ def show_map(ll_spn=None, map_type="map", add_params=None):
     pygame.quit()
     # Удаляем за собой файл с изображением.
     os.remove(map_file)
+
+
+def return_map(ll_spn=None, map_type="map", add_params=None):
+    if ll_spn:
+        map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
+    else:
+        map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
+
+    if add_params:
+        map_request += "&" + add_params
+
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "data/map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    return [pygame.image.load(map_file), os.remove(map_file)][0]
